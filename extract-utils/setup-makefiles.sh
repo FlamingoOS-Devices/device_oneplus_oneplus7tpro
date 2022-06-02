84e3b4791786df2fbffa1c1d32d543a0e9f40e70
Pranav <npv12@iitbbs.ac.in>
2022-06-02 13:33:05 +0530

    oneplus7tpro: Move extarct related files to separate dir

--- extract-utils/setup-makefiles.sh
+++ extract-utils/setup-makefiles.sh
@@ -6,16 +6,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# If we're being sourced by the common script that we called,
-# stop right here. No need to go down the rabbit hole.
-if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
-    return
-fi
-
 set -e
 
 export DEVICE=oneplus7tpro
 export DEVICE_COMMON=sm8150-common
 export VENDOR=oneplus
 
-"./../../${VENDOR}/${DEVICE_COMMON}/extract-files.sh" "$@"
+"./../../../${VENDOR}/${DEVICE_COMMON}/extract-utils/setup-makefiles.sh" "$@"
